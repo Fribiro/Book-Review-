e13c38c0beccd55966e246ef2bf3f79a4f5beff1
fribiro <festusribiro@gmail.com>
2022-03-06 20:56:35 +0300

    Created docker files for the client, server and root. Created and setup the docker-compose.yml

--- server/index.js
+++ server/index.js
@@ -19,9 +19,49 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hi There')
+app.get('/getBooks', (req, res) => {
+    const SelectQuery = "SELECT * FROM books_reviews";
+    db.query(SelectQuery, (err,result) => {
+        res.send(result);
+    });
 });
+
+app.post('/insertBook', (req,res) => {
+    // const newBook = {
+    //     bookName: req.body.setBookName,
+    //     bookReview: req.body.setReview,
+    // };
+    const bookName = req.body.setBookName;
+    const bookReview = req.body.setReview;
+    console.log(req.body.setBookName);
+    
+
+    const InsertQuery = "INSERT INTO books_reviews (book_name, book_review) VALUES (?, ?)";
+    db.query(InsertQuery, [bookName, bookReview], (err, result) => {
+        res.send(`New Book added to the database successfully. + ${result}`)
+    });
+});
+
+app.delete('/deleteBook/:bookId', (req,res) => {
+    const bookId = req.params.bookId;
+    const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
+    db.query(DeleteQuery, bookId, (err, result) => {
+        if (err) {
+            throw (err)
+        } else {
+            res.send(`Book successfully deleted. + ${result}`)
+        }
+    })
+})
+
+app.put("/updateBook/:bookId", (req, res) => {
+    const bookReview = req.body.reviewUpdate;
+    const bookId = req.params.bookId;
+    const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
+    db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
+        if (err) console.log(err)
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
